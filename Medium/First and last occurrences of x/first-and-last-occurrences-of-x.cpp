0836b00c94e4ac8c6f064a7b3437d8632ba568e6
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-09-22 19:16:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    vector<int> find(int arr[], int n , int x )
+    {
+        // code here
+        if(!binary_search(arr,arr+n,x)) return {-1,-1};
+        
+        int first=0,last=n-1;
+        
+        first = lower_bound(arr,arr+n,x)-arr;
+        
+        last=upper_bound(arr,arr+n,x)-(arr+1);
+        
+        return {first,last};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
