7c5cddbe208380a62935318c2f9ebdda2efe24f1
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-11-30 23:38:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Shortest path from 1 to n/shortest-path-from-1-to-n.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+       int minimumStep(int n){
+        //complete the function here
+        int ans = 0;
+        while(n > 1){
+            if(n == 2){ans++; break;}
+            if(n%3 != 0){
+                ans += n%3;
+                n = n - n%3;
+            }
+            else{
+                n /= 3;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minimumStep(n) << endl;
+    
+    }
+    return 0; 
+} 
+
+
+// } Driver Code Ends
