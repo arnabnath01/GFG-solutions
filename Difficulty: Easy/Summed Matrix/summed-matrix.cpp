2960561cc77d729061aeb924978720b43d9d79a4
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-06-24 21:27:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        long long maxi =n+n;
+        if(q<=1 || q>maxi) return 0;
+        
+        if(q>maxi/2) return maxi-q+1;
+        else return q-1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
