dd85780918c442879f1d4369fba3f6f523a8c494
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-07-01 23:45:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make Binary Tree From Linked List/make-binary-tree-from-linked-list.cpp
@@ -0,0 +1,136 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void push(struct Node **head_ref, int new_data) {
+
+    struct Node *new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void lvl(TreeNode *r) {
+    if (r == NULL)
+        return;
+    queue<TreeNode *> q;
+    q.push(r);
+    while (!q.empty()) {
+        TreeNode *j = q.front();
+        cout << j->data << " ";
+        q.pop();
+        if (j->left)
+            q.push(j->left);
+        if (j->right)
+            q.push(j->right);
+    }
+}
+
+void reverses(Node **head) {
+    Node *prev = NULL;
+    Node *cur = *head;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *head = prev;
+}
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+   void convert(Node* head, TreeNode*& root) {
+    if (!head) return;  // If the linked list is empty, return
+    
+    root = new TreeNode(head->data);  // Initialize root with the head's data
+    Node* curr = head->next;  // Start with the next node in the list
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    // Creation of Tree from Linked List
+    while (curr) {
+        TreeNode* temp = q.front();
+        q.pop();
+        
+        // Left Node
+        if (curr) {
+            temp->left = new TreeNode(curr->data);
+            q.push(temp->left);
+            curr = curr->next;
+        }
+        
+        // Right Node
+        if (curr) {
+            temp->right = new TreeNode(curr->data);
+            q.push(temp->right);
+            curr = curr->next;
+        }
+    }
+}
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T, i, n, l, k;
+
+    cin >> T;
+
+    while (T--) {
+        struct Node *head = NULL;
+
+        cin >> n;
+        for (i = 1; i <= n; i++) {
+            cin >> l;
+            push(&head, l);
+        }
+        reverses(&head);
+        Node *prevhead = head;
+        TreeNode *root = NULL;
+        Solution obj;
+
+        obj.convert(head, root);
+        if (root == NULL) {
+            cout << "-1\n";
+        } else if (root == NULL && prevhead != NULL) {
+            cout << "-1\n";
+        } else {
+            lvl(root);
+            cout << endl;
+        }
+        // inorder(root);
+        getchar();
+    }
+    return 0;
+}
+
+// } Driver Code Ends
