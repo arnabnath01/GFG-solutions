8953816ecb7a798a2badbf05eb4f9c2d361ed655
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-07-18 00:22:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -6,38 +6,93 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution {
+
+
+class DisJointSet{
+
 public:
+vector<int> parent,rank,size;
+    DisJointSet(int n){
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+
+   
+   // time complexity: O(4*alpha) ~ O(4) ~ O(1
+    int find(int u){
+        if(parent[u]==u){
+            return u;
+        }
+        return parent[u]=find(parent[u]);    // path compression
+    }
+
+
+
+    void Rank(int u,int v){
+        int up=find(u);
+        int vp=find(v);
+        if(up==vp) return ;
+
+        if(rank[up]<rank[vp]){
+            parent[up]=vp;
+        }
+        else if(rank[up]>rank[vp]){
+            parent[vp]=up;
+        }
+        else{   // equal rank
+        parent[up]=vp;
+        rank[vp]++;
+        }
+
+    }
+
+
+        void Size(int u,int v){
+        int up=find(u);
+        int vp=find(v);
+        if(up==vp) return ;
+        
+        if(size[up]<size[vp]){
+            parent[up]=vp;
+            size[vp]+=size[up];
+        }
+        else{
+            parent[vp]=up;
+            size[up]+=size[vp];
+        }
+        
+        }
+
+};
+
+
+class Solution {
+  public:
     int numProvinces(vector<vector<int>> adj, int V) {
-        vector<int> adjls[V];
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (adj[i][j] != 0 && i != j) {
-                    adjls[i].push_back(j);
-                    adjls[j].push_back(i);
+        // code here
+        DisJointSet ds(V);
+        
+        for(int i=0;i<V;i++)
+        {
+            for(int j=0;j<V;j++)
+            {
+                if(adj[i][j]==1){
+                    ds.Size(i,j);
                 }
             }
         }
         
-        int v[V] = {0};
-        int c = 0;
-        for (int i = 0; i < V; i++) {
-            if (v[i] != 1) {
-                c++;
-                dfs(i, v, adjls);
-            }
+        int cnt=0;
+        for(int i=0;i<V;i++){
+            if(ds.parent[i]==i) cnt++;
         }
-        return c;
-    }
-    
-private:
-    void dfs(int node, int v[], vector<int> adjls[]) {
-        v[node] = 1;
-        for (auto it : adjls[node]) {
-            if (v[it] != 1) {
-                dfs(it, v, adjls);
-            }
-        }
+        return cnt;
+        
     }
 };
 
