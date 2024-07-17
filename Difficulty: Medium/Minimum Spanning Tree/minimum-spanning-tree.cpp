5ae13678cfd0b7a50df83b69748a5d92b06aafeb
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-07-17 23:21:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -6,49 +6,117 @@ using namespace std;
 
 
 
+
+
+
+class DisJointSet{
+
+vector<int> parent,rank,size;
+
+public:
+    DisJointSet(int n){
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+
+   
+   // time complexity: O(4*alpha) ~ O(4) ~ O(1
+    int findUPar(int u){
+        if(parent[u]==u){
+            return u;
+        }
+        return parent[u]=findUPar(parent[u]);    // path compression
+    }
+
+
+
+    void unionByRank(int u,int v){
+        int up=findUPar(u);
+        int vp=findUPar(v);
+        if(up==vp) return ;
+
+        if(rank[up]<rank[vp]){
+            parent[up]=vp;
+        }
+        else if(rank[up]>rank[vp]){
+            parent[vp]=up;
+        }
+        else{   // equal rank
+        parent[up]=vp;
+        rank[vp]++;
+        }
+
+    }
+
+
+        void unionBySize(int u,int v){
+        int up=findUPar(u);
+        int vp=findUPar(v);
+        if(up==vp) return ;
+        
+        if(size[up]<size[vp]){
+            parent[up]=vp;
+            size[vp]+=size[up];
+        }
+        else{
+            parent[vp]=up;
+            size[up]+=size[vp];
+        }
+        
+        }
+
+};
+
+
+
+
+
+
+
+
+
 class Solution
 {
-    public:
-    //Function to find sum of weights of edges of the Minimum Spanning Tree.
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        vector<pair<int,int>> mst;
-        int ans = 0;
-        // wt, node
-        priority_queue<
-        pair<int,int>,
-        vector<pair<int,int>>,
-        greater<pair<int,int>>
-        > q;
-        
-        vector<bool> vis(V, false);
-        q.push({0, 0});  // Start with node 0
-        
-        while(!q.empty())
+        vector<pair<int,pair<int,int>>>v;
+        for(int i=0;i<V;i++)
         {
-            int wt = q.top().first;
-            int node = q.top().second;
-            q.pop();
-            
-            if(vis[node]) continue;
-            
-            // Mark visited when adding to the MST
-            vis[node] = true;
-            ans += wt;
-            
-            for(auto& edge : adj[node])
+            for(auto it:adj[i])
             {
-                int adjNode = edge[0];
-                int adjWt = edge[1];
-                if(!vis[adjNode])
-                {
-                    q.push({adjWt, adjNode});
-                }
+                int adjNode=it[0];
+                int wt=it[1];
+                int node =i;
+                v.push_back({wt,{node,adjNode}});
             }
+            
         }
         
-        return ans;
+        sort(v.begin(),v.end());
+        DisJointSet ds(V);
+        int mst=0;
+        for(auto it:v){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            
+            if(ds.findUPar(u)!=ds.findUPar(v)){
+                mst+=wt;
+                ds.unionBySize(u,v);
+            }
+            
+        }
+        
+        return mst;
+        
     }
 };
 
