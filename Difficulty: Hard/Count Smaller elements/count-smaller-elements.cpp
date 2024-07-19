b416f9e660f1c976c6fd6ab353785ebbc6601c04
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-07-19 14:23:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Smaller elements/count-smaller-elements.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+//  User function template for C++
+
+class Solution {
+  public:
+    vector<int> constructLowerArray(vector<int> &arr) {
+        // code here
+       int n = arr.size();
+    vector<int> ans(n, 0);
+    vector<int> temp;
+
+    for (int i = n - 1; i >= 0; i--) {
+        auto it = lower_bound(temp.begin(), temp.end(), arr[i]);
+        ans[i] = distance(temp.begin(), it); // Calculate the number of elements strictly less than arr[i]
+        temp.insert(it, arr[i]); // Insert arr[i] in the sorted position
+    }
+
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        vector<int> a = ob.constructLowerArray(arr);
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
