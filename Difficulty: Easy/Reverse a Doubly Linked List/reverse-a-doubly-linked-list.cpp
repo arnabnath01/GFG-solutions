33b3fb27b30aefc38fbe04a0882ec28edf76dda8
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-08-07 17:41:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.cpp
@@ -0,0 +1,167 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+void displayList(Node *head)
+{
+    //Head to Tail
+    while(head->next)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+    
+    
+    
+}
+
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+
+bool verify(Node* head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev)
+    {
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+class Solution
+{
+    public:
+    Node* reverseDLL(Node * head)
+    {
+        //Your code here
+        Node* temp = NULL;  //helper pointer for swapping prev and next pointers.
+        Node* curr = NULL;  //pointer for the current node.
+        
+
+curr= head;
+        while(curr!=NULL)
+        {
+            if(curr->next==NULL)  //To check if it's last node, to update head.
+            {
+                head = curr;
+            }
+            temp = curr->prev;
+            curr->prev = curr->next;
+            curr->next=temp;
+            curr=curr->prev;   //pointing curr to the next node.
+        }
+        return head;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail=NULL;
+        int x;
+	    cin>>x;
+	    head = newNode(x);
+	    tail = head;
+	    
+	    for(int i=0;i<n - 1;i++)
+	    {
+	        cin>>x;
+	        Node* temp=newNode(x);
+	        tail->next=temp;
+	        temp->prev= tail;
+	        tail = temp;
+	    }
+	    Solution ob;
+	    head=ob.reverseDLL(head);
+	    
+	    
+	    if(verify(head))
+	    displayList(head);
+	    else
+	    cout<<"Your pointers are not correctly connected";
+ 
+	    cout<<endl;
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
